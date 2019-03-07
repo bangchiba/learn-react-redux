cfa27a309a9ab89a3ff6a93ac606dafffff09705
Khairul Bahri <chibaxp.tech@gmail.com>
2019-03-07 11:15:30 +0700

    update redux create new post

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -1,7 +1,6 @@
 import { FETCH_POSTS, NEW_POST } from "./types";
 
 export const fetchPosts = () => dispatch => {
-    console.log('oke');
     fetch('https://jsonplaceholder.typicode.com/posts')
         .then(res => res.json())
         .then(posts => 
@@ -10,4 +9,19 @@ export const fetchPosts = () => dispatch => {
                 payload: posts
         })
     );
+}
+
+export const createPost = PostData => dispatch => {
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(PostData)
+    })
+        .then(res => res.json())
+        .then(post => dispatch({
+            type: NEW_POST,
+            payload: post
+        }));
 }
